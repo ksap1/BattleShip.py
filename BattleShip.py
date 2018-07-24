1a8a5eb74b7d01a82c8d064ddc3a0e84bb67cf4f
ksap1 <kylesaltandpepper@gmail.com>
2018-07-24 01:12:20 -0400

    Commit #4, More Strategic CPU Selections

--- BattleShip.py
+++ BattleShip.py
@@ -60,31 +60,37 @@ def shipmaker(map, numtimes):
                         map[i][col] = letter
                     break
 
+
 def start_game(userboard, board, preboard, cpuboard):
     sleep(7)
-    shipcount = 0
+    # cpu guessing variables:
+    count = 0
+    firsthit = False  # Becomes True if hit ship once, becomes false once ship is sunken
+    shipdestroy = False  # Becomes True when ship is continuing to be destroyed
+    counthit = 0  # if greater than 1, shipdestroy becomes True
+    # end of cpu guessing variables
     while True:
         sleep(3)
         print("\nYour targets:\n")
         print_board(board)
-        while True: #to see if the coordinate was never hit
+        while True:  # to see if the coordinate was never hit
             try:
-                userrow = int(input("\nEnter a row 1-10: "))-1
-                usercol = int(input("\nEnter a column 1-10: "))-1
+                userrow = int(input("\nEnter a row 1-10: ")) - 1
+                usercol = int(input("\nEnter a column 1-10: ")) - 1
             except ValueError:
                 print("Try Again")
             hit = preboard[userrow][usercol]
             if hit == "#" or hit == "X":
                 print("You already hit that spot. Try Again")
-            else: break
-
+            else:
+                break
         if hit != "*":
             preboard[userrow][usercol] = "#"
             board[userrow][usercol] = "X"
             if any(hit in line for line in preboard) == False:
                 print("\nYou hit and sunk a ship!")
-                shipcount+=1
-            else: print("\nYou hit!")
+            else:
+                print("\nYou hit!")
 
         else:
             print("\nYou missed!")
@@ -92,43 +98,97 @@ def start_game(userboard, board, preboard, cpuboard):
         tots = 0
         for t in range(len(preboard)):
             for y in preboard[t]:
-                if y == "#" or y=="*":
-                    tots+=1
-        sleep(1)
-        print("\n"+str(shipcount)+" out of 4 ships sunken")
-        if tots==100:
+                if y == "#" or y == "*":
+                    tots += 1
+        if tots == 100:
             print("\n\nYou Win!")
-            break #if all ships were sunken
+            break  # if all ships were sunken
         sleep(3)
         print("\n\n\nYour Map:")
         print_board(userboard)
         sleep(1)
         print("\n Waiting for your opponent's move......\n")
         sleep(3)
-        #computer's turn
-        count = 0
+        # computer's turn Let's begin
+
         while True:
-            cpurow,cpucol = randint(0,9),randint(0,9)
-            rand = cpuboard[cpurow][cpucol]
-            if rand != "#":
-                break
-        cpuboard[cpurow][cpucol] = "#"
+            if firsthit == False:
+                cpurow, cpucol = randint(0, 9), randint(0, 9)
+                rand = cpuboard[cpurow][cpucol]
+                if rand != "#":
+                    break
+            else:
+
+                possibledir = [[cpurow + 1, cpucol], [cpurow - 1, cpucol], [cpurow, cpucol + 1], [cpurow, cpucol - 1]]
+                if shipdestroy == True:  # continue destruction if target hit 2x in a row
+                    if huh <= 1:  # in this if/else statement, it makes the count continue based on the direction the 2nd hit was made
+                        if huh == 0:
+                            possibledir[huh][
+                                0] = cpurow + counthit  # counthit is pretty much the counter (+=1) for the amount of hits before a ship is sunk(then counthit=0)
+                        else:
+                            possibledir[huh][0] = cpurow - counthit
+                    else:
+                        if huh == 2:
+                            possibledir[huh][1] = cpucol + counthit
+                        else:
+                            possibledir[huh][1] = cpucol - counthit
+                    try:
+                        if cpuboard[possibledir[huh][0]][possibledir[huh][1]] != "#":
+                            rand = cpuboard[possibledir[huh][0]][possibledir[huh][1]]
+                            break
+                        else:
+                            shipdestroy = False
+                    except ValueError:
+                        while True:
+                            cpurow, cpucol = randint(0, 9), randint(0, 9)
+                            rand = cpuboard[cpurow][cpucol]
+                            if rand != "#":
+                                break
+                if shipdestroy == False:  # first target was hit, now just starting to find next target
+                    while True:
+                        huh = randint(0, 3)
+                        try:
+                            rand = cpuboard[possibledir[huh][0]][possibledir[huh][1]]
+                        except IndexError:
+                            cpurow, cpucol = randint(0, 9), randint(0,
+                                                                    9)  # if suspected ship coordinate is out of range, it'll just choose a random coordinate
+                            rand = cpuboard[cpurow][cpucol]
+                        if rand != "#":
+                            break
+                    break
+
+        if firsthit == False:
+            cpuboard[cpurow][cpucol] = "#"
+        else:
+            cpuboard[possibledir[huh][0]][possibledir[huh][1]] = "#"
+
+        # Now to check the coordinates of the computer
         if rand != "*":
-            count+=1
+            counthit += 1
+            count += 1
+            if firsthit == False:
+                userboard[cpurow][cpucol] = "X"
+            else:
+                userboard[possibledir[huh][0]][possibledir[huh][1]] = "X"
             if any(rand in bob for bob in userboard) == False:
                 print("\nYou got hit and your ship sunk!")
-            else: print("\nYou got hit!")
-            userboard[cpurow][cpucol] = "X"
-        else: print("\nYour opponent missed")
+                counthit = 0
+                firsthit = False
+                shipdestroy = False
+            else:
+                print("\nYou got hit!")
+                firsthit = True
+                if counthit > 1:
+                    shipdestroy = True
+
+        else:
+            print("\nYour opponent missed")
+            shipdestroy = False
+
         if count == 14:
-            print("\n\nyou lost!")
+            sleep(2)
+            print("\n\nYou lost!")
             break
-
-
-
-
-
-
 
 
 def print_board(boad):
